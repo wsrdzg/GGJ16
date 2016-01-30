cb718d620eaed8f007b93f2bae17d9c081e65471
floatec <mp@floatec.de>
2016-01-30 13:24:54 +0100

    factory

--- core/src/ggj16/ka/bluetooth/QuestFactory.java
+++ core/src/ggj16/ka/bluetooth/QuestFactory.java
@@ -6,9 +6,33 @@ import com.badlogic.gdx.utils.Array;
  * Created by floatec on 30/01/16.
  */
 public class QuestFactory {
+
+    public static Array<God> gods;
+
+    static {
+        gods = new Array<God>();
+        gods.add(new God("Apu","God or spirit of mountains.","God"));
+        gods.add(new God("Cavillace","Virgin Goddess","Goddess"));
+        gods.add(new God("Chasca","Goddess of dawn and twilight and Venus.","Goddess"));
+        gods.add(new God("Coniraya","Moon God.","God"));
+        gods.add(new God("Ekkeko","God of the hearth and wealth.","God"));
+        gods.add(new God("Illapa","Weather God","God"));
+        gods.add(new God("Inti","Sun God","God"));
+        gods.add(new God("Kon","God of rain.","God"));
+        gods.add(new God("Kuka Mama","Goddess of health and joy.","Goddess"));
+        gods.add(new God("Mama Cocha","Sea and fish goddess, protectress of sailors and fishermen.","Goddess"));
+        gods.add(new God("Mama Quilla","Goodess of marriage, festival and the moon.","Goddess"));
+                gods.add(new God("Mama Zara","Goddess of grain.","Goddess"));
+        gods.add(new God("Pacha Camac","Chthonic creator god.","God"));
+        gods.add(new God("Pariacaca","God of water and rainstorms.","God"));
+        gods.add(new God("Supay","God of death.","God"));
+        gods.add(new God("Urcuchillay","God that watched over animals.","God"));
+        gods.add(new God("Viracocha","God of everything.","God"));
+    }
+
     public static Quest getQuest(int questNumer, Array<Symbol> symbols){
         Quest quest = new Quest();
-        quest.id = "Kill Xardas";
+        quest.id = "Portal of "+gods.get(questNumer).name;
         quest.maxTime = 5000;
         quest.symbols = new Array<>();
         quest.symbols.add(symbols.get((187*(questNumer+9))%symbols.size));
