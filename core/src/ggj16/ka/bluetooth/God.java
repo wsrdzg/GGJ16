466e429cdcc0fc9aa482e3f5455977f898c9458d
floatec <mp@floatec.de>
2016-01-30 14:59:29 +0100

    spells

--- core/src/ggj16/ka/bluetooth/God.java
+++ core/src/ggj16/ka/bluetooth/God.java
@@ -4,11 +4,13 @@ package ggj16.ka.bluetooth;
  * Created by floatec on 30/01/16.
  */
 public class God {
-    String name,description,type;
-    public God(String name,String description,String type){
+    String name,description,type,spell,spellDescrition;
+    public God(String name,String description,String type,String spell,String spellDescrition){
         this.name=name;
         this.description=description;
         this.type=type;
+        this.spell=spell;
+        this.spellDescrition=spellDescrition;
 
     }
 }
